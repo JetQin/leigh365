009244ef05ce315c32e479dcbefff09d761dbcf2
ChenPei <315534868@qq.com>
2017-12-21 10:43:19 +0800

    fix searchScreen bug

--- src/screens/search/SearchScreen.js
+++ src/screens/search/SearchScreen.js
@@ -43,6 +43,9 @@ class SearchScreen extends Component {
 
   }
   doSearch() {
+    if (!this.state.searchValue){
+      return;
+    }
     if (this.state.searchType === '搜行情') {
       // this.searchStock();
       this.state.stock.page = 1;
@@ -116,21 +119,27 @@ class SearchScreen extends Component {
 
     return (
       <View style={{ flex: 1 }}>
-        <View style={{ marginTop: '4%', flexDirection: 'row', borderBottomColor: Colors.$navigationHeaderTextColor, borderBottomWidth: 3 }}>
-          <Image source={require('../../../assets/imgs/logo.png')} style={styles.logo} />
-          <View style={styles.searchBar}>
-            <TextInput
-              placeholder="请输入关键词 按Enter搜索"
-              underlineColorAndroid='transparent'
-              value={this.state.searchValue}
-              onChangeText={(searchValue) => this.setState({ searchValue })}
-              style={styles.searchInput}
-            />
-            <Button transparent onPress={this.doSearch} style={{ position: 'absolute', right: '12%' }}>
+        <View style={styles.container}>
+          <View style={styles.logoContainer}>
+            <Image source={require('../../../assets/imgs/logo.png')} style={styles.logo} />
+          </View>
+          <View style={styles.searchContainer}>
+            <View style={styles.inputContainer}>
+              <TextInput
+                placeholder="请输入关键词 按Enter搜索"
+                underlineColorAndroid='transparent'
+                value={this.state.searchValue}
+                onChangeText={(searchVal) => this.setState({ searchValue: searchVal })}
+                style={styles.searchInput}
+              />
+            </View>
+            <Button transparent onPress={this.doSearch} style={{right:'10%'}}>
               <Icon type='ionicon' name="ios-search" size={20} />
             </Button>
-            <Button transparent onPress={() => this.props.navigation.goBack()} style={styles.closeBtn}>
-              <Icon type='font-awesome' name="close" size={20} color={Colors.$navigationHeaderTextColor} />
+          </View>
+          <View style={styles.closeContainer}>
+            <Button transparent style={styles.closeBtn} onPress={() => this.props.navigation.goBack()} >
+              <Icon type='font-awesome'  name="close" size={20} color={Colors.$navigationHeaderTextColor} />
             </Button>
           </View>
         </View>
--- src/screens/search/styles/SearchScreen.js
+++ src/screens/search/styles/SearchScreen.js
@@ -1,23 +1,33 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 const styles = EStyleSheet.create({
+  container: {
+    flexDirection: 'row', 
+    borderBottomColor: '$navigationHeaderTextColor', 
+    borderBottomWidth: 3,
+  },
+  logoContainer: {
+    flex: 0.1,
+  },
+  searchContainer: {
+    flex: 0.8,
+    flexDirection: 'row', 
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  closeContainer: {
+    flex: 0.1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputContainer: {
+    flex: 1,
+  },
   logo: {
     top: 15,
     left: 10,
     width: 20,
     height: 20,
-  },
-  titleText: {
-    left: 15,
-    fontSize: 18,
-    paddingTop: 12,
-    fontFamily: 'Montserrat-Regular',
-    fontWeight: 'bold',
-  },
-  stockContainer: {
-    flex: 1,
-    marginTop: 5,
-    marginBottom: 5,
   },
   searchInput: {
     borderRadius: 20, 
@@ -27,30 +37,20 @@ const styles = EStyleSheet.create({
     height: 30, 
     borderWidth: 1, 
     borderColor: '#ccc',
-    width: '85%',
-    backgroundColor: '#EEEEEE', 
   },
-  searchBar: {
-    width: '92%', 
-    flexDirection: 'row', 
-    marginLeft: '2%', 
-    alignItems: 'center', 
-    position: 'relative' 
-  },
-  closeBtn: {
-    position: 'absolute', 
-    right: '1%', 
-    top: '18%', 
+  closeBtn: { 
     width: 30, 
     height: 30, 
     borderRadius: 15, 
-    backgroundColor: '#EFF8FF'
+    backgroundColor: '#EFF8FF',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  closeIcon: {
-    fontSize: 20, 
-    position: 'absolute', 
-    left: '18%', 
-    color: '$navigationHeaderTextColor',
+
+  stockContainer: {
+    flex: 1,
+    marginTop: 5,
+    marginBottom: 5,
   }
 
 });
