6f1489c22adb49d15991aebee960e0893b7ee119
dangjinling_1012@126.com <dangjinling_1012@126.com>
2017-12-22 17:04:16 +0800

    SEARCH SCREEN BUG FIX

---
+++ src/screens/search/components/styles/StockList.js
@@ -0,0 +1,105 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+import Colors from '../../../../../constants/Colors';
+
+const styles = EStyleSheet.create({
+  root: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: Colors.$blackBlueColor,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth: 1,
+  },
+  redBorder: {
+    borderLeftWidth: 5,
+    borderStyle: 'solid',
+    borderLeftColor: Colors.$redColor,
+  },
+  greenBorder: {
+    borderLeftWidth: 5,
+    borderStyle: 'solid',
+    borderLeftColor: Colors.$redColor,
+  },
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  headerLeft: {
+    flex: 0.3,
+  },
+  headerCenter: {
+    flex: 0.2,
+  },
+  headerRight: {
+    flex: 0.2,
+    alignItems: 'flex-end',
+  },
+  buttonRight: {
+    flex: 0.3,
+  },
+  headerText: {
+    fontSize: 18,
+    fontFamily: 'Montserrat-Regular',
+    color: Colors.$black,
+    backgroundColor: 'transparent',
+  },
+  headerRedText: {
+    marginLeft: 10,
+    fontSize: 18,
+    fontFamily: 'Montserrat-Regular',
+    color: Colors.$redColor,
+    backgroundColor: 'transparent',
+  },
+  headerGreenText: {
+    marginLeft: 10,
+    fontSize: 18,
+    fontFamily: 'Montserrat-Regular',
+    color: Colors.$greenColor,
+    backgroundColor: 'transparent',
+  },
+  footer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  footerLeft: {
+    flex: 0.3,
+  },
+  footerCenter: {
+    flex: 0.2,
+  },
+  footerRight: {
+    flex: 0.2,
+    flexDirection: 'row',
+  },
+  footerText: {
+    fontSize: 12,
+    fontFamily: 'Montserrat-Regular',
+    color: Colors.$black,
+    backgroundColor: 'transparent',
+  },
+  footerRedText: {
+    marginLeft: 2,
+    fontSize: 18,
+    fontFamily: 'Montserrat-Regular',
+    color: Colors.$redColor,
+    backgroundColor: 'transparent',
+  },
+  footerGreenText: {
+    marginLeft: 2,
+    fontSize: 18,
+    fontFamily: 'Montserrat-Regular',
+    color: Colors.$greenColor,
+    backgroundColor: 'transparent',
+  },
+  icon: {
+    marginLeft: 20,
+  },
+  separator: {
+    flex: 1,
+    height: 3,
+    backgroundColor: Colors.$blackBlueColor,
+  },
+
+});
+
+export default styles;
